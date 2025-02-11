04ac66f87627ab0ccfa3be44c14efb78276ef0c8
asgharale <asgharale11@gmail.com>
2025-02-11 20:17:17 +0330

    user api endpoints lognin & sign up

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,6 +1,18 @@
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
+    path('api/user/v1/', include("user.urls")),
     path('admin/', admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs'
+    )
 ]
